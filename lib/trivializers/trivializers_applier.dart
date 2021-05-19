a79bc9ab9a9e2a7b2e59aae1ec70a48801a1bdc2
Evols <hutteau.b@gmail.com>
2021-05-19 03:08:51 +0200

    SingleChildTrivializer

--- lib/trivializers/trivializers_applier.dart
+++ lib/trivializers/trivializers_applier.dart
@@ -1,6 +1,7 @@
 
 import 'package:formula_transformator/trivializers/mult_one_trivializer.dart';
 import 'package:formula_transformator/trivializers/mult_zero_trivializer.dart';
+import 'package:formula_transformator/trivializers/single_child_trivializer.dart';
 import 'package:formula_transformator/trivializers/trivializer.dart';
 import 'package:formula_transformator/utils.dart';
 import 'package:formula_transformator/values/value.dart';
@@ -11,6 +12,7 @@ const trivializers = <Trivializer>[
   AddZeroTrivializer(),
   MultOneTrivializer(),
   MultZeroTrivializer(),
+  SingleChildTrivializer(),
 ];
 
 Value apply_trivializers(Value value) {
