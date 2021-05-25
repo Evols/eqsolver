427fe4b14f46074dc80e8973217ca82b2574498c
Evols <hutteau.b@gmail.com>
2021-05-25 22:58:11 +0200

    Equation

---
+++ lib/core/equation.dart
@@ -0,0 +1,9 @@
+
+import 'package:formula_transformator/core/values/value.dart';
+
+class Equation {
+  final Value leftPart;
+  final Value rightPart;
+
+  Equation(this.leftPart, this.rightPart);
+}
