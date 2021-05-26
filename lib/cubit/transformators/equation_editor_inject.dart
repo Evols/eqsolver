9ac4a793be7e4294d6b139937e70c7aec5699f80
Evols <hutteau.b@gmail.com>
2021-05-26 22:02:40 +0200

    Can't inject constants

--- lib/cubit/transformators/equation_editor_inject.dart
+++ lib/cubit/transformators/equation_editor_inject.dart
@@ -2,6 +2,7 @@
 import 'package:formula_transformator/core/equation.dart';
 import 'package:formula_transformator/core/equation_transformators/inject_transformator.dart';
 import 'package:formula_transformator/core/trivializers/trivializers_applier.dart';
+import 'package:formula_transformator/core/values/constant.dart';
 import 'package:formula_transformator/core/values/multiplication.dart';
 import 'package:formula_transformator/core/values/value.dart';
 import 'package:formula_transformator/cubit/equation_editor_cubit.dart';
@@ -39,7 +40,7 @@ class EquationEditorInject extends EquationEditorEditing {
         (part) => identical(part, value) || part is Multiplication && part.factors.where(
           (factor) => identical(factor, value)
         ).isNotEmpty
-      ).isNotEmpty) {
+      ).isNotEmpty && !(value is Constant)) {
         return (
           identical(sourceExpression, value)
           ? Selectable.SingleSelected
