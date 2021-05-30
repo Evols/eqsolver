68e55690577f0bf4a4766e2a7762f1f48e453c7c
Evols <hutteau.b@gmail.com>
2021-05-30 08:56:31 +0200

    Intervals classes

---
+++ lib/core/value_sets/value_set.dart
@@ -0,0 +1,10 @@
+
+import 'package:formula_transformator/core/value_sets/value_interval.dart';
+
+class ValueSet {
+
+  final List<ValueIntervalUnion> intervals;
+
+  const ValueSet(this.intervals);
+
+}
